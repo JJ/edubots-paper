b0f4749a0f2443bb04b194ac486b6a7beddc4ca0
JJ Merelo <jjmerelo@gmail.com>
2021-04-28 10:13:59 +0200

    Deletes invalid response

--- scripts/process.R
+++ scripts/process.R
@@ -1,38 +1,16 @@
 
-data.u <- read.csv("../data/encuesta-edubots-uni.csv")
-data.c <- read.csv("../data/encuesta-edubots-ciclos.csv")
-
-data.u$Marca.temporal <- NULL
-data.c$Marca.temporal <- NULL
-
-data.c <- data.c[ data.c$X.Imparte.docencia.en.ciclos.formativos.de.grado.medio.o.superior. == "Sí", ]
-
-data.c$X.Imparte.docencia.en.ciclos.formativos.de.grado.medio.o.superior. <- NULL
-
-data.c$Sector <- "Vocational"
-data.u$Sector <- "University"
-
-data <- rbind(data.c, data.u)
-
-names(data)[names(data) == "Titulaciones.que.imparte"] <- "Degree"
-names(data)[names(data) == "Área"] <- "Area"
-names(data)[names(data) == "Edad..años."] <- "Age"
-names(data)[names(data) == "Experiencia.docente"] <- "Experience"
-names(data)[names(data) == "Sobre.el.uso.de.los.sistemas.de.mensajería.en.clase..prefiero"] <- "Messaging.Preference"
-names(data)[names(data) == "X.Qué.plataforma.de.mensajería.GRUPAL.usas.normalmente.para.comunicarte.con.tus.compañeros.y.estudiantes."] <- "Messaging.Platform"
-names(data)[names(data) == "X.Qué.tipo.de.bots..programas.que.respondan.automáticamente.a.preguntas.u.órdenes...podrían.resultar.útiles.para.mejorar.los.resultados.de.aprendizaje.de.los.estudiantes.o.ayudarte..en.general..a.gestionar.una.asignatura."] <- "Chatbots.Preference"
-names(data)[names(data) == "La.virtualización.de.la.enseñanza.debida.al.COVID.este.año.o.el.anterior...ha.significado.algún.cambio.en.tu.percepción.o.uso.de.mensajería.en.clase."] <- "COVID.Changes"
-
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-library(stringr)
+library(scales)
 
-data$Messaging.Platform <- str_replace_all(data$Messaging.Platform, " específica de la universidad, titulación o departamento", "" )
-data$Messaging.Platform <- str_replace_all(data$Messaging.Platform, "Ninguna, prefiero", "Ninguna prefiero" )
-data <- data %>% separate_rows( Messaging.Platform, sep="," )
-data$Messaging.Platform <- trimws( data$Messaging.Platform )
+data <- read.csv("../data/survey-pilot-3.csv")
 
-data.messaging <- data %>% group_by(Messaging.Platform) %>% mutate(count_platforms = n())
-data.messaging <- data.messaging[ data.messaging$count_platforms > 1, ]
-ggplot( data.messaging, aes( x = reorder(Messaging.Platform, -count_platforms ), group=Sector )) + geom_bar( stat="count" ) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggplot(data, aes(x = WhatsApp, group = Sector)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Sector)
+ggplot(data, aes(x = Telegram, group = Sector)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Sector)
+ggplot(data, aes(x = Provided, group = Sector)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Sector)
+
+ggplot(data, aes(x = WhatsApp, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
+ggplot(data, aes(x = Telegram, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
+ggplot(data, aes(x = Provided, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
+q
