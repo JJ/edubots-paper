8f26018faec9fa525e76ebe1854d3bd1afbf773c
Francisco Barranco <fbarranco@ugr.es>
2021-05-19 20:17:28 +0200

    Updates data and scripts for covid impact refs #12

--- scripts/translate.py
+++ scripts/translate.py
@@ -25,6 +25,11 @@ data = data.replace("Humanidades", "Humanities")
 data = data.replace("Ingeniería", "Engineering")
 data = data.replace("Tecnología", "Technology")
 
+#PostCOVID Changes
+data = data.replace("No, antes la usaba y ahora la sigo usando.", "No, I did use it before, and use it now")
+data = data.replace("Sí, antes no usaba ningún tipo de plataforma de mensajería y ahora sí la uso.", "Yes, I didn't use any messaging app before, but do use it now")
+data = data.replace("Sí, he cambiado de plataforma de mensajería a otras más seguras.", "Yes, I use a safer messaging app now")
+data = data.replace("No, no la usaba antes ni ahora.", "No, I didn't use it before, and don't use it now")
 
 with open("../data/survey-pilot-3-EN.csv","w") as file:
      file.write(data)
