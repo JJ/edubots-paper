43603379b1ad9a195155ed268dd78a3834a34ced
Francisco Barranco <fbarranco@ugr.es>
2021-05-20 18:05:49 +0200

    Updates data and scripts for chat groups use refs #12

--- scripts/translate.py
+++ scripts/translate.py
@@ -31,5 +31,16 @@ data = data.replace("Sí, antes no usaba ningún tipo de plataforma de mensajer�
 data = data.replace("Sí, he cambiado de plataforma de mensajería a otras más seguras.", "Yes, I use a safer messaging app now")
 data = data.replace("No, no la usaba antes ni ahora.", "No, I didn't use it before, and don't use it now")
 
+
+#Messaging.Organization
+#Un grupo con los estudiantes de mi asignatura
+#Prefiero que los estudiantes se autoorganicen y no estar en ningún grupo de mensajería
+#Un grupo para el curso completo (profesores y estudiantes)
+#Un grupo para toda la titulación, Facultad o Escuela (profesores y estudiantes) 
+data = data.replace("Prefiero que los estudiantes se autoorganicen y no estar en ningún grupo de mensajería", "I'd rather leave students self-organize and not be part of the chat")
+data = data.replace("Un grupo con los estudiantes de mi asignatura", "A chat with my students in the same course")
+data = data.replace("Un grupo para el curso completo (profesores y estudiantes)", "A chat with all students and teachers in the same semester/year")
+data = data.replace("Un grupo para toda la titulación, Facultad o Escuela (profesores y estudiantes)", "A chat with all the students and teachers in my Faculty or School")
+
 with open("../data/survey-pilot-3-EN.csv","w") as file:
      file.write(data)
