dcc0c7a456f98b5e48124e123cb969d4c25f8172
Francisco Barranco <fbarranco@ugr.es>
2021-05-23 13:54:52 +0200

    Translates interaction survey refs #12

--- scripts/process-tutorship.R
+++ scripts/process-tutorship.R
@@ -4,7 +4,19 @@ library(tidyr)
 library(scales)
 library(ggthemes)
 
-data <- read.csv("../data/survey-student-interaction-pilot-3-2021.csv")
+data <- read.csv("../data/survey-student-interaction-pilot-3-2021-EN.csv")
+
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~factor(Gender, levels=c('Female','Male','PNTS'))) + theme(axis.title.y=element_blank())
+ggsave('../figures/gender_interaction.pdf', width = 100, height = 70, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~Age) + theme(axis.title.y=element_blank())
+ggsave('../figures/age_interaction.pdf', width = 100, height = 70, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Experience)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~Experience) + theme(axis.title.y=element_blank())
+ggsave('../figures/experience_interaction.pdf', width = 100, height = 70, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Discipline)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~factor(Discipline, levels=c('Biomedicine','Humanities','Social Sciences', "Sciences", "Tech/Engineering","No"))) + theme(axis.title.y=element_blank())
+ggsave('../figures/discipline_interaction.pdf', width = 186, height = 77, units = "mm")
+
+
+
 
 ggplot(data, aes(x = Familiarity, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
 ggplot(data, aes(x = Horizontality, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
--- scripts/translate.py
+++ scripts/translate.py
@@ -33,10 +33,6 @@ data = data.replace("No, no la usaba antes ni ahora.", "No, I didn't use it befo
 
 
 #Messaging.Organization
-#Un grupo con los estudiantes de mi asignatura
-#Prefiero que los estudiantes se autoorganicen y no estar en ningún grupo de mensajería
-#Un grupo para el curso completo (profesores y estudiantes)
-#Un grupo para toda la titulación, Facultad o Escuela (profesores y estudiantes) 
 data = data.replace("Prefiero que los estudiantes se autoorganicen y no estar en ningún grupo de mensajería", "I'd rather leave students self-organize and not be part of the chat")
 data = data.replace("Un grupo con los estudiantes de mi asignatura", "A chat with my students in the same course")
 data = data.replace("Un grupo para el curso completo (profesores y estudiantes)", "A chat with all students and teachers in the same semester/year")
@@ -44,4 +40,40 @@ data = data.replace("Un grupo para toda la titulación o instituto (profesores y
 data = data.replace("Un grupo para toda la titulación, Facultad o Escuela (profesores y estudiantes)", "A chat with all the students and teachers in my Faculty/School")
 
 with open("../data/survey-pilot-3-EN.csv","w") as file:
+     file.write(data)
+
+
+
+
+data = ""
+
+with open("../data/survey-student-interaction-pilot-3-2021.csv") as file:
+     data = file.read()
+
+#Age
+data = data.replace("Más de 55", "> 55")
+
+#Experience 
+data = data.replace("0-5 años", "0-05 years")
+data = data.replace("5-15 años", "06-15 years")
+data = data.replace("15-25 años", "16-25 years")
+data = data.replace("más de 25 años", "> 25 years")
+
+#Gender
+data = data.replace("Hombre", "Male")
+data = data.replace("Mujer", "Female")
+data = data.replace("Prefiero no decirlo", "PNTS")
+
+#Discipline
+data = data.replace("Biosanitaria", "Biomedicine")
+data = data.replace("Ciencias Sociales", "Social Sciences")
+data = data.replace("Ciencias", "Sciences")
+data = data.replace("Humanidades", "Humanities")
+data = data.replace("Tecnología/Ingeniería", "Tech/Engineering")
+
+#Interaction
+data = data.replace("Asíncrona: el estudiante puede preguntar en cualquier momento, el profesorado contesta cuando puede.", "Asynch.")
+data = data.replace("Síncrona: estudiantes(s) y profesor(es) en el mismo espacio, simultáneamente.", "Synch.")
+
+with open("../data/survey-student-interaction-pilot-3-2021-EN.csv","w") as file:
      file.write(data)
