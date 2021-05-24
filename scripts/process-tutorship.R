0c18545c9ed57e561be4dc80571da9866bab651c
Francisco Barranco <fbarranco@ugr.es>
2021-05-24 16:18:34 +0200

    adds the question and section/results for RQ3 refs #16

--- scripts/process-tutorship.R
+++ scripts/process-tutorship.R
@@ -6,17 +6,20 @@ library(ggthemes)
 
 data <- read.csv("../data/survey-student-interaction-pilot-3-2021-EN.csv")
 
-ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~factor(Gender, levels=c('Female','Male','PNTS'))) + theme(axis.title.y=element_blank())
-ggsave('../figures/gender_interaction.pdf', width = 100, height = 70, units = "mm")
-ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~Age) + theme(axis.title.y=element_blank())
-ggsave('../figures/age_interaction.pdf', width = 100, height = 70, units = "mm")
-ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Experience)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~Experience) + theme(axis.title.y=element_blank())
-ggsave('../figures/experience_interaction.pdf', width = 100, height = 70, units = "mm")
-ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Discipline)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + xlab("Type of interaction") + ylab("relative frequencies") + facet_grid(~factor(Discipline, levels=c('Biomedicine','Humanities','Social Sciences', "Sciences", "Tech/Engineering","No"))) + theme(axis.title.y=element_blank())
-ggsave('../figures/discipline_interaction.pdf', width = 186, height = 77, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~factor(Gender, levels=c('Female','Male','PNTS'))) + theme(axis.title.y=element_blank(), axis.title.x=element_blank())
+ggsave('../figures/gender_synch.pdf', width = 100, height = 70, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age) + theme(axis.title.y=element_blank(), axis.title.x=element_blank())
+ggsave('../figures/age_synch.pdf', width = 100, height = 70, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Experience)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Experience) + theme(axis.title.y=element_blank(), axis.title.x=element_blank())
+ggsave('../figures/experience_synch.pdf', width = 100, height = 70, units = "mm")
+ggplot(data, aes(x = Interaction.Synchrony..or.not., group = Discipline)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count", show.legend = FALSE) + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~factor(Discipline, levels=c('Biomedicine','Humanities','Social Sciences', "Sciences", "Tech/Engineering","No"))) + theme(axis.title.y=element_blank(), axis.title.x=element_blank())
+ggsave('../figures/discipline_synch.pdf', width = 186, height = 77, units = "mm")
 
-
-
+ggplot(data, aes(x = Familiarity, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
+ggplot(data, aes(x = Horizontality, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
+ggplot(data, aes(x = Connectivity, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
+ggplot(data, aes(x = Sustainability, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
+ggplot(data, aes(x = OfficialFormation, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
 
 ggplot(data, aes(x = Familiarity, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
 ggplot(data, aes(x = Horizontality, group = Age)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Age)
@@ -36,10 +39,5 @@ ggplot(data, aes(x = Connectivity, group = Discipline)) + geom_bar(aes(y = ..pro
 ggplot(data, aes(x = Sustainability, group = Discipline)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Discipline)
 ggplot(data, aes(x = OfficialFormation, group = Discipline)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Discipline)
 
-ggplot(data, aes(x = Familiarity, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
-ggplot(data, aes(x = Horizontality, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
-ggplot(data, aes(x = Connectivity, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
-ggplot(data, aes(x = Sustainability, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
-ggplot(data, aes(x = OfficialFormation, group = Gender)) + geom_bar(aes(y = ..prop.., fill = factor(..x..)), stat="count") + scale_y_continuous(labels=scales::percent) + ylab("relative frequencies") + facet_grid(~Gender)
 
 
