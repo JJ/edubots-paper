609672444b086dc1eade5bedc38530fd12cdb85f
Francisco Barranco <fbarranco@ugr.es>
2021-06-20 09:59:57 +0200

    adds script for spss analysis refs #24

---
+++ scripts/parse_spss.py
@@ -0,0 +1,32 @@
+data = ""
+
+with open("../data/survey-student-interaction-pilot-3-2021.csv") as file:
+     data = file.read()
+
+#Age
+data = data.replace("Más de 55", "> 55")
+
+#Experience 
+data = data.replace("0-5 años", "0-05 years")
+data = data.replace("5-15 años", "06-15 years")
+data = data.replace("15-25 años", "16-25 years")
+data = data.replace("más de 25 años", "> 25 years")
+
+#Gender
+data = data.replace("Hombre", "Male")
+data = data.replace("Mujer", "Female")
+data = data.replace("Prefiero no decirlo", "PNTS")
+
+#Discipline
+data = data.replace("Biosanitaria", "Biomedicine")
+data = data.replace("Ciencias Sociales", "Social Sciences")
+data = data.replace("Ciencias", "Sciences")
+data = data.replace("Humanidades", "Humanities")
+data = data.replace("Tecnología/Ingeniería", "Tech/Engineering")
+
+#Interaction
+data = data.replace("Asíncrona: el estudiante puede preguntar en cualquier momento, el profesorado contesta cuando puede.", "Asynch.")
+data = data.replace("Síncrona: estudiantes(s) y profesor(es) en el mismo espacio, simultáneamente.", "Synch.")
+
+with open("../data/survey-student-interaction-pilot-3-2021-EN.csv","w") as file:
+     file.write(data)
